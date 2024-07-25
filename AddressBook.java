27bbaed72f6d5738a18dadfceba01b15aabcbda0
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-07-25 22:44:23 +0530

    Creating Edit Method

--- AddressBook.java
+++ AddressBook.java
@@ -53,6 +53,58 @@ public class AddressBook {
         if(!fl)
             System.out.println("There is no file having name : "+name);
     }
+
+    public void edit(String name){
+        boolean fl=false;
+        for(Contacts gg:list) {
+            if(Objects.equals(gg.firstName, name) || Objects.equals(gg.lastName, name)) {
+                fl = true;
+                System.out.println("Which field you Want to change");
+                System.out.println("1. Address");
+                System.out.println("2. City");
+                System.out.println("3. State");
+                System.out.println("4. Zip");
+                System.out.println("5. Number");
+                System.out.println("6. Email");
+                Scanner sc=new Scanner(System.in);
+                int option = sc.nextInt();
+
+                Scanner sc1 = new Scanner(System.in);
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter the New Address: ");
+                        gg.address= sc1.nextLine();
+                        break;
+                    case 2:
+                        System.out.println("Enter the New City: ");
+                        gg.city= sc1.nextLine();
+                        break;
+                    case 3:
+                        System.out.println("Enter the New State: ");
+                        gg.state= sc1.nextLine();
+                        break;
+                    case 4:
+                        System.out.println("Enter the New Zip");
+                        gg.zip=sc.nextInt();
+                        break;
+                    case 5:
+                        System.out.println("Enter the New Number");
+                        gg.number= sc.nextLong();
+                        break;
+                    case 6:
+                        System.out.println("Enter the New Email");
+                        gg.email= sc1.nextLine();
+                        break;
+                    default:
+                        System.out.println("Enter Valid option");
+                        break;
+                }
+
+            }
+        }
+        if (!fl)
+            System.out.println("There is no file having name : " + name);
+    }
     }
 
 
