b9cbc821f7937bec06dc2ff6c3cb3d8ff6f40e73
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-07-25 17:35:37 +0530

    Create method is created in Addressbook

---
+++ AddressBook.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class AddressBook {
+    ArrayList<Contacts> list=new ArrayList<>();
+
+    public void create(){
+        Contacts newContact = new Contacts();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your First Name");
+        newContact.firstName=sc.nextLine();
+        System.out.println("Enter your Last Name");
+        newContact.lastName= sc.nextLine();
+        System.out.println("Enter your Address");
+        newContact.address= sc.nextLine();
+        System.out.println("Enter your City");
+        newContact.city= sc.nextLine();
+        System.out.println("Enter your State");
+        newContact.state= sc.nextLine();
+        System.out.println("Enter your Zip Code");
+        Scanner sc1=new Scanner(System.in);
+        newContact.zip= sc1.nextInt();;
+        System.out.println("Enter your Number");
+        newContact.number= sc1.nextLong();
+        System.out.println("Enter your Email");
+        newContact.email= sc.nextLine();
+
+        list.add(newContact);
+    }
+
+    public Contacts read(String name){
+        System.out.println("read file");
+        for(Contacts gg:list) {
+            if (Objects.equals(gg.firstName, name) || Objects.equals(gg.lastName, name)) {
+                System.out.println("found");
+                return gg;
+            }
+        }
+
+            Contacts n=new Contacts();
+            return n;
+
+    }
+
+}
