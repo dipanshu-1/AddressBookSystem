917480078e4cf430ed446a57318e5f7ab1669aef
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-07-25 18:12:44 +0530

    contact file is needed

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class AddressBookMain
 {
     public static void main(String[] args) {
         System.out.println("Welocme to addressbook");
+
+        AddressBook func= new AddressBook();
+        func.create();
+        String name="Dipanshu";
+        System.out.println(func.read(name));
+
     }
 }
